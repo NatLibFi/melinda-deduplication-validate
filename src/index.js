256c8199153175900acfd010081151ece06cbe2d
Pasi Tuominen <pasi.tuominen@gmail.com>
2017-07-21 13:36:52 +0300

    Adds implementation for reading the queue and checking duplicate-ness with marc-record-similarity

---
+++ src/index.js
@@ -0,0 +1,88 @@
+// @flow
+
+const logger = require('melinda-deduplication-common/utils/logger');
+logger.log('info', 'Starting melinda-deduplication-validate');
+
+const _ = require('lodash');
+const amqp = require('amqplib');
+
+const utils = require('melinda-deduplication-common/utils/utils');
+const CandidateQueueService = require('melinda-deduplication-common/utils/candidate-queue-service');
+const DuplidateQueueConnector = require('melinda-deduplication-common/utils/duplicate-queue-connector');
+const DataStoreService = require('melinda-deduplication-common/utils/data-store-service');
+
+const CANDIDATE_QUEUE_AMQP_HOST = utils.readEnvironmentVariable('CANDIDATE_QUEUE_AMQP_HOST');
+const DUPLICATE_QUEUE_AMQP_HOST = utils.readEnvironmentVariable('DUPLICATE_QUEUE_AMQP_HOST');
+const DATASTORE_API = utils.readEnvironmentVariable('DATASTORE_API', 'http://localhost:8080');
+
+const dataStoreService = DataStoreService.createDataStoreService(DATASTORE_API);
+const RecordSimilarityService = require('./record-similarity-service');
+
+start().catch(error => {
+  logger.log('error', error.message, error);
+});
+
+async function start() {
+  logger.log('info', 'Connecting to rabbitMQ');
+  const candidateQueueConnection = await amqp.connect(CANDIDATE_QUEUE_AMQP_HOST);
+  const channel = await candidateQueueConnection.createChannel();
+  logger.log('info', 'Connected to rabbitMQ');
+  const candidateQueueService = CandidateQueueService.createCandidateQueueService(channel);
+
+  const duplicateQueueConnection = await amqp.connect(DUPLICATE_QUEUE_AMQP_HOST);
+  const duplicateChannel = await duplicateQueueConnection.createChannel();
+  const duplicateQueueConnector = DuplidateQueueConnector.createDuplicateQueueConnector(channel);
+
+  candidateQueueService.listenForCandidates(async (candidate, done) => {
+
+    logger.log('info', 'Loading records from data store');
+    const startTime = process.hrtime();
+    const ioStart = process.hrtime();
+    const firstRecord = await dataStoreService.loadRecord(candidate.first.base, candidate.first.id);
+    const secondRecord = await dataStoreService.loadRecord(candidate.second.base, candidate.second.id);
+    const ioDuration = utils.hrtimeToMs(process.hrtime(ioStart));
+
+    logger.log('info', 'Checking record similarity');
+    const validateStart = process.hrtime();
+
+    let validationResult;
+    try {
+      validationResult = RecordSimilarityService.checkSimilarity(firstRecord, secondRecord);
+    } catch(e) {
+      logger.log('error', 'Failure in marc-record-similarity module, skipping this candidate');
+      logger.log('error', candidate);
+      return done();
+    }
+    const validateDuration = utils.hrtimeToMs(process.hrtime(validateStart));
+
+    console.log(validationResult);
+
+    const {IS_DUPLICATE, NOT_DUPLICATE, MAYBE_DUPLICATE} = RecordSimilarityService.DuplicateClass;
+
+    if (validationResult.type === MAYBE_DUPLICATE || validationResult.type === IS_DUPLICATE) {
+      console.log(firstRecord.toString());
+      console.log(secondRecord.toString());
+    }
+
+    switch(validationResult.type) {
+      case IS_DUPLICATE: await sendToDuplicateQueue(candidate, validationResult); break;
+      case MAYBE_DUPLICATE: await sendToDuplicateDatabase(candidate); break;
+    }
+
+    const duration = utils.hrtimeToMs(process.hrtime(startTime));
+    logger.log('info', `Candidate was handled in ${duration}ms - IO took ${ioDuration}ms - Validation took ${validateDuration}ms`);
+
+    done();
+
+    async function sendToDuplicateQueue(candidate, validationResult) {
+      const duplicate = _.extend({}, candidate, { probability: validationResult.numeric});
+      duplicateQueueConnector.pushDuplicate(duplicate);
+    }
+
+    async function sendToDuplicateDatabase(candidate) {
+      logger.log('info', 'Sending MAYBE to duplicate queue for testing, this should go to duplicate db');
+      await sendToDuplicateQueue(candidate, validationResult);
+    }
+  });
+
+}
---
+++ src/record-similarity-service.js
@@ -0,0 +1,46 @@
+// @flow
+
+const fs = require('fs');
+const path = require('path');
+const RecordSimilarity = require('marc-record-similarity');
+const defaultStrategy = require('marc-record-similarity/strategies/all');
+
+const net = fs.readFileSync(path.resolve(__dirname, '../node_modules/marc-record-similarity/neural/networks/2014-11-27.json'), 'utf8');
+
+const options = {
+  network: JSON.parse(net),
+  strategy: defaultStrategy
+};
+
+const similarity = new RecordSimilarity(options);
+
+const DuplicateClass = {
+  IS_DUPLICATE: 'IS_DUPLICATE',
+  NOT_DUPLICATE: 'NOT_DUPLICATE',
+  MAYBE_DUPLICATE: 'MAYBE_DUPLICATE'
+};
+
+function checkSimilarity(firstRecord, secondRecord) {
+
+  const result = similarity.check(firstRecord, secondRecord);
+
+  return {
+    type: classifyResult(result),
+    numeric: result
+  };
+}
+
+function classifyResult(validationResult) {
+  if (validationResult < 0.5) {
+    return DuplicateClass.NOT_DUPLICATE;
+  }
+  if (validationResult > 0.95) {
+    return DuplicateClass.IS_DUPLICATE;
+  }
+  return DuplicateClass.MAYBE_DUPLICATE;
+}
+
+module.exports = {
+  checkSimilarity,
+  DuplicateClass
+};
